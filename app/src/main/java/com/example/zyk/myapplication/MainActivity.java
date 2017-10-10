4fc1ad8f78115649732f96a623eec785a4c84def
wbw <wbw@gmail.com>
2017-10-10 21:21:49 +0800

    wbw test3

--- app/src/main/java/com/example/zyk/myapplication/MainActivity.java
+++ app/src/main/java/com/example/zyk/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("ceshi tijiao");
-        System.out.println("wbw ceshi tijio");
+        System.out.println("wbw ceshi tijiohj");
     }
 }
